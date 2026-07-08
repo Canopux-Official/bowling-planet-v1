5c97d5395c82920a46e6329b8232641ba2558b2e
Archit <architmishra016@gmail.com>
2026-07-08 12:17:32 +0530

    Update App.tsx routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,8 +9,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import LandingPage from './pages/LandingPage'
 import AboutPage from './pages/AboutPage'
-import PortfolioPage from './pages/PortfolioPage'
-import ServicesPage from './pages/ServicesPage'
 import ProductsPage from './pages/ProductsPage'
 import FranchisePage from './pages/FranchisePage'
 import CareersPage from './pages/CareersPage'
@@ -26,16 +24,13 @@ export default function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<LandingPage />} />
           <Route path="about" element={<AboutPage />} />
-          <Route path="portfolio" element={<PortfolioPage />} />
-          <Route path="services" element={<ServicesPage />} />
           <Route path="products" element={<ProductsPage />} />
           <Route path="franchise" element={<FranchisePage />} />
           <Route path="careers" element={<CareersPage />} />
           <Route path="contact" element={<ContactPage />} />
           <Route path="blog" element={<BlogPage />} />
         </Route>
-        
-        {/* Auth routes don't use the main Layout (no standard Nav/Footer) */}
+
         <Route path="login" element={<LoginPage />} />
         <Route path="signup" element={<SignupPage />} />
       </Routes>
